fe8e51e47ed0979f22343478626c961e7c8bf5db
ThinamXx <thinamjr1@gmail.com>
2024-03-18 03:20:33 +0000

    💻 testing the translation

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 
-MODEL_NAME = "t5-small"
+MODEL_NAME = "Thinam/lang-translate"
+
 
 def load_model(model_name):
     """
@@ -12,13 +13,14 @@ def load_model(model_name):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     return model, tokenizer
 
+
 def translate(text, model, tokenizer, target_language):
     """
     Translate the input text to the target language.
     """
     try:
         text = f"{target_language} {text}"
-        inputs = tokenizer.encode(text, return_tensors='pt')
+        inputs = tokenizer.encode(text, return_tensors="pt")
         outputs = model.generate(inputs)
         translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
         return translated_text
@@ -26,28 +28,28 @@ def translate(text, model, tokenizer, target_language):
         st.error(f"Translation failed: {e}")
         return ""
 
-def main():
-    st.title('Language Translation App')
 
-    # load the model and tokenizer
+def main():
+    st.title("Language Translation App")
+
     model, tokenizer = load_model(MODEL_NAME)
 
-    target_language = st.selectbox("Select target language", ["French", "Romanian", "German"])
-    text = st.text_input("Enter text to translate")
+    target_language = "German"
 
-    st.markdown('<style>body{background-color: white}</style>', unsafe_allow_html=True)
-    st.markdown('<style>h1{color: #336699;}</style>', unsafe_allow_html=True)
-    st.markdown('<style>label{color: #336699;}</style>', unsafe_allow_html=True)
-    st.markdown('<style>button{background-color: #336699; color: white;}</style>', unsafe_allow_html=True)
+    input_text = st.text_area("Enter text to translate", height=200)
+    output_text = st.empty()
 
     col1, col2 = st.columns(2)
+
     with col1:
-        if st.button('Submit'):
-            translated_text = translate(text, model, tokenizer, target_language)
-            st.text_area("Translated Text", translated_text, height=200)
+        if st.button("Submit"):
+            translated_text = translate(input_text, model, tokenizer, target_language)
+            output_text.text_area("Translated Text", translated_text, height=200)
+
     with col2:
-        if st.button('Clear'):
-            st.empty()
+        if st.button("Clear"):
+            output_text.empty()
+
 
 if __name__ == "__main__":
     main()
